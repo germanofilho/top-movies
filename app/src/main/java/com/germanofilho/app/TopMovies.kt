d0f5f826c79ad22893941ed045cac5b654921219
germanofilho <germanof16@gmail.com>
2019-05-02 20:30:36 -0300

    :wrench: Implementing Koin

---
+++ app/src/main/java/com/germanofilho/app/TopMovies.kt
@@ -0,0 +1,18 @@
+package com.germanofilho.app
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class TopMovies : Application() {
+    override fun onCreate(){
+        super.onCreate()
+        // start Koin!
+        startKoin {
+            // Android context
+            androidContext(this@TopMovies)
+            // modules
+            //modules(myModule)
+        }
+    }
+}
